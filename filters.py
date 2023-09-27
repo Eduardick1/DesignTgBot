3e67f348522021ce8713176a55e0979c4af62f93
Eduardick1 <ed.sushh@gmail.com>
2023-09-27 14:37:06 +0300

    Railway 1.0

---
+++ filters.py
@@ -0,0 +1,7 @@
+from typing import Any
+from aiogram.filters import BaseFilter
+from aiogram.types import Message
+from aiogram.fsm.context import FSMContext
+from typing import Callable, Awaitable, Dict, Any
+
+
