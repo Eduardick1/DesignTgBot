8af773e36ae862e755337c42cc7e4e6fa45fd33f
Eduard <47057625+Eduardick1@users.noreply.github.com>
2023-09-27 15:54:30 +0300

    Update DataBase.py

--- DataBase.py
+++ DataBase.py
@@ -9,6 +9,6 @@ client = MongoClient(os.getenv('uri_db'))
 db = client.designerBot
 coll = db.users_id
 
-redis_reg = Redis(host="containers-us-west-185.railway.app", password="Dz459YIDQ0Z8rZYp3FAj", port=5812, username='default', db=0, decode_responses=True)
-redis_bot = Redis(host="containers-us-west-185.railway.app", password="Dz459YIDQ0Z8rZYp3FAj", port=5812, username='default', db=1, decode_responses=True)
+redis_reg = Redis(db=0, decode_responses=True)
+redis_bot = Redis(db=1, decode_responses=True)
 
