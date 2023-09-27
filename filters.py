efe2eb4f9bfa205c74a4892a09647c3c1a528295
Eduard <47057625+Eduardick1@users.noreply.github.com>
2023-09-27 15:59:29 +0300

    Delete filters.py

--- filters.py
+++
@@ -1,7 +0,0 @@
-from typing import Any
-from aiogram.filters import BaseFilter
-from aiogram.types import Message
-from aiogram.fsm.context import FSMContext
-from typing import Callable, Awaitable, Dict, Any
-
-
